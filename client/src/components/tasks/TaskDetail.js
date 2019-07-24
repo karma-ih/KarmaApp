a8b43a9a0964dac01acef2c226fe45c6a2b148c9
Alican Torun <Alican@Ironhacks-MacBook-Pro.local>
2019-07-24 17:48:29 +0200

    task detail and go back to task copleted

--- client/src/components/tasks/TaskDetail.js
+++ client/src/components/tasks/TaskDetail.js
@@ -5,8 +5,31 @@ import { Link, Redirect } from "react-router-dom";
 class TaskDetail extends Component {
   state = {};
 
+  componentDidMount() {
+    this.getSingleTask();
+  }
+
+  getSingleTask = () => {
+    const { params } = this.props.match;
+    axios
+      .get(`http://localhost:5555/api/tasks/${params.id}`)
+      .then(responseFromApi => {
+        const theTask = responseFromApi.data;
+        this.setState(theTask);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   render() {
-    return <div>Welcome to the Task Details Page</div>;
+    return (
+      <div>
+        <h1>{this.state.title}</h1>
+        <p>{this.state.description}</p>
+        <Link to={"/tasks"}>Back to Tasks</Link>
+      </div>
+    );
   }
 }
 

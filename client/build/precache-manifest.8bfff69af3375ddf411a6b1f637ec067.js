ee382a2c674be60e8d7a4c83aa004457203c0b7e
aniruddh-jain <ani@blockseed.co>
2019-08-01 15:02:12 +0200

    build pushed to tree

---
+++ client/build/precache-manifest.8bfff69af3375ddf411a6b1f637ec067.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92510d6f679339e16d4755ca8aafda68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60a65e63a0218ebf6c74",
+    "url": "/static/css/2.bfe7958e.chunk.css"
+  },
+  {
+    "revision": "5400c33faa4b7915680d",
+    "url": "/static/css/main.097711be.chunk.css"
+  },
+  {
+    "revision": "60a65e63a0218ebf6c74",
+    "url": "/static/js/2.14ff622c.chunk.js"
+  },
+  {
+    "revision": "5400c33faa4b7915680d",
+    "url": "/static/js/main.f93b429b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);

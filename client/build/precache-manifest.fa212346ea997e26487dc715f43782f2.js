74cef452eceda3488930bb04c0362c26b0bc5881
domgdsman <dominik.gadesmann@gmail.com>
2019-08-02 10:04:44 +0200

    master min magic

---
+++ client/build/precache-manifest.fa212346ea997e26487dc715f43782f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51e40a34cb64bd9da6e4ce1cc217bde6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5bb2526a5b89260e9de",
+    "url": "/static/css/2.bfe7958e.chunk.css"
+  },
+  {
+    "revision": "d50737d6966b9d0015b3",
+    "url": "/static/css/main.e227fbf3.chunk.css"
+  },
+  {
+    "revision": "f5bb2526a5b89260e9de",
+    "url": "/static/js/2.8c23cae6.chunk.js"
+  },
+  {
+    "revision": "d50737d6966b9d0015b3",
+    "url": "/static/js/main.d35bc5da.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
